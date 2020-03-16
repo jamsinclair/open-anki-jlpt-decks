90674de19b5084fe3350b6fe64f0a4d296ed75d2
jamsinclair <jamsinclairnz@gmail.com>
2020-03-16 22:47:52 +1300

    Add server and scripts for taking card screenshots

---
+++ scripts/templates-screenshot.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async() => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 320,
+    height: 350,
+    deviceScaleFactor: 1,
+  });
+  await page.goto('http://localhost:3000/question');
+  await page.screenshot({path: 'screenshots/question.png'});
+  await page.goto('http://localhost:3000/answer');
+  await page.screenshot({path: 'screenshots/answer.png'});
+  await browser.close();
+})();
