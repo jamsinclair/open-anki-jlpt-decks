597dc931a7cf848c30dfb7bd0fbbc4f0d666b7fd
jamsinclair <jamsinclairnz@gmail.com>
2020-03-18 00:33:18 +1300

    Format and lint js files

--- scripts/templates-screenshot.js
+++ scripts/templates-screenshot.js
@@ -1,19 +1,21 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
-(async() => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+const init = async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
   await page.setViewport({
     width: 320,
     height: 350,
-    deviceScaleFactor: 1,
-  });
-  await page.goto('http://localhost:3000/question');
-  await page.screenshot({path: 'screenshots/question.png'});
-  await page.goto('http://localhost:3000/answer');
-  await page.screenshot({path: 'screenshots/answer.png'});
-  await browser.close();
-})();
+    deviceScaleFactor: 1
+  })
+  await page.goto('http://localhost:3000/question')
+  await page.screenshot({ path: 'screenshots/question.png' })
+  await page.goto('http://localhost:3000/answer')
+  await page.screenshot({ path: 'screenshots/answer.png' })
+  await browser.close()
+}
+
+init()
--- scripts/templates-serve.js
+++ scripts/templates-serve.js
@@ -1,33 +1,37 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const express = require('express');
-const app = express();
-const port = 3000;
+const fs = require('fs')
+const express = require('express')
+const app = express()
+const port = 3000
 
-const questionTemplate = fs.readFileSync('./src/templates/question.html');
-const answerTemplate = fs.readFileSync('./src/templates/answer.html');
-const styles = fs.readFileSync('./src/templates/styles.css');
+const questionTemplate = fs.readFileSync('./src/templates/question.html')
+const answerTemplate = fs.readFileSync('./src/templates/answer.html')
+const styles = fs.readFileSync('./src/templates/styles.css')
 
-const renderCardHtml = content => (`
+const renderCardHtml = content => `
 <!doctype html>
 <title>Card</title>
 <style>html,body { height: 100%; margin: 0; }</style>
 <style>${styles}</style>
 <div class="card">${content}</div>
-`);
+`
 
 app.get('/question', (req, res) => {
-  const content = questionTemplate.toString().replace('{{expression}}', '生きる');
-  res.send(renderCardHtml(content));
-});
+  const content = questionTemplate
+    .toString()
+    .replace('{{expression}}', '生きる')
+  res.send(renderCardHtml(content))
+})
 
 app.get('/answer', (req, res) => {
-  const content = answerTemplate.toString().replace('{{expression}}', '生きる')
+  const content = answerTemplate
+    .toString()
+    .replace('{{expression}}', '生きる')
     .replace('{{reading}}', 'いきる')
-    .replace('{{meaning}}', 'to live');
-  res.send(renderCardHtml(content));
-});
+    .replace('{{meaning}}', 'to live')
+  res.send(renderCardHtml(content))
+})
 
-app.listen(port, () => console.log(`Templates app listening on port ${port}!`));
+app.listen(port, () => console.log(`Templates app listening on port ${port}!`))
